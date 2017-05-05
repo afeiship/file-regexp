9938217fd88160843ec8b604939f46ba12ee3bbc
afeiship <1290657123@qq.com>
2017-05-05 12:36:43 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+const CHARSET = 'utf-8';
+
+function fileGetContent(inFile,inRegRe){
+  let str = fs.readFileSync(inFile,CHARSET);
+  return str.match(inRegRe)[1];
+}
+
+function fileReplaceContent(inFile,inRegRe,inString){
+  let str = fs.readFileSync(inFile,CHARSET);
+  str = str.replace(inRegRe,inString);
+  fs.writeFileSync(inFile,str);
+}
+
+export {
+  fileGetContent,
+  fileReplaceContent
+};
